52c3c45899b45d00ba508d953cc2143922b46f20
Mark Xue <mark@mxue.org>
2017-05-18 14:26:54 -0700

    calls library get server tmp key

--- sslScanGo.go
+++ sslScanGo.go
@@ -13,10 +13,44 @@ import "C"
 import (
 
     "log"
+    // "fmt"
+    "time"
     // "crypto/tls"
     "github.com/spacemonkeygo/openssl"
     // "github.com/davecgh/go-spew/spew"
 )
+
+const rsaCiphers = "AES128-SHA256:AES256-SHA256:AES128-GCM-SHA256:AES256-GCM-SHA384"
+
+var dhCiphers = []string{"DH-RSA-AES128-SHA256", 
+                           "DH-RSA-AES256-SHA256",
+                           "DH-RSA-AES128-GCM-SHA256",
+                           "DH-RSA-AES256-GCM-SHA384",
+
+                           "DHE-RSA-AES128-SHA256",
+                           "DHE-RSA-AES256-SHA256",
+                           "DHE-RSA-AES128-GCM-SHA256",
+                           "DHE-RSA-AES256-GCM-SHA384",
+
+                           "DHE-DSS-AES128-SHA256",
+                           "DHE-DSS-AES256-SHA256",
+                           "DHE-DSS-AES128-GCM-SHA256",
+                           "DHE-DSS-AES256-GCM-SHA384",
+
+                           "ECDHE-RSA-AES128-SHA256",
+                           "ECDHE-RSA-AES256-SHA384",
+                           "ECDHE-RSA-AES128-GCM-SHA256",
+                           "ECDHE-RSA-AES256-GCM-SHA384",
+
+                           "ECDHE-ECDSA-AES128-SHA256",
+                           "ECDHE-ECDSA-AES256-SHA384",
+                           "ECDHE-ECDSA-AES128-GCM-SHA256",
+                           "ECDHE-ECDSA-AES256-GCM-SHA384",
+
+                           "ADH-AES128-SHA256",
+                           "ADH-AES256-SHA256",
+                           "ADH-AES128-GCM-SHA256",
+                           "ADH-AES256-GCM-SHA384"}
 
 type sslCheckOptions struct {
   host string
@@ -32,7 +66,7 @@ func (s sslCheckOptions) testProtocolCipher (cipherName string) {
   const request = "GET / HTTP/1.0\r\nUser-Agent: SSLScan\r\nHost: %s\r\n\r\n"
 
   //creates TLS context (SSL disabled by default)
-  context, err := openssl.NewCtx()
+  context, err := openssl.NewCtxWithVersion(0x06)
   if err != nil {
           log.Fatal(err)
   }
@@ -44,7 +78,9 @@ func (s sslCheckOptions) testProtocolCipher (cipherName string) {
 
   conn, err := openssl.Dial("tcp", s.host, context, 0)
   if err != nil {
-    log.Fatal(err)
+    log.Print(err)
+    return
+    //fmt.Sprintf("%s rejected cipher %s", s.host, cipherName)
   }
   defer conn.Close()
 
@@ -54,9 +90,22 @@ func (s sslCheckOptions) testProtocolCipher (cipherName string) {
   if err != nil {
     log.Fatal(err)
   }
+  //fmt.Sprintf("%s accepted cipher %s", s.host, sslCipherName)
   println(sslCipherName)
+  conn.PrintServerTmpKey()
 
 
+}
+
+func (s sslCheckOptions) testProtocolCiphers (limiter  <-chan time.Time) {
+  <-limiter
+  s.testProtocolCipher(rsaCiphers)
+
+  for _, cipher := range dhCiphers {
+     <-limiter
+     // fmt.Println("Tick at", time.Now())
+     go s.testProtocolCipher(cipher)
+  }
 
 }
 
@@ -71,7 +120,10 @@ func testHost() {
 }
 
 func main() {
-  options := sslCheckOptions{"www.yahoo.com:443" , 443}
-  options.testProtocolCiphers("DHE-RSA-AES256-SHA256")
+  hostname := "dh1024.badssl.com:443"
+  options := sslCheckOptions{ hostname, 443}
+
+  limiter := time.Tick(time.Millisecond * 500)
+  options.testProtocolCiphers(limiter)
 
 }
