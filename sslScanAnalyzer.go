222656fe32f8eb35dfaca3f6b2f583560734d3d4
Mark Xue <mark@mxue.org>
2017-05-26 12:42:31 -0700

    building copy with table init

--- sslScanAnalyzer.go
+++ sslScanAnalyzer.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-  "encoding/json"
-  "github.com/mattn/go-sqlite3"
-  "io/ioutil"
+  // "encoding/json"
+  "database/sql"
+  // "fmt"
+  _ "github.com/mattn/go-sqlite3"
+  // "io/ioutil"
   "log"
   "os"
 )
@@ -22,7 +24,7 @@ const (
   other
 )
 
-func check(e Error) {
+func check(e error) {
   if e != nil {
     log.Fatal(e)
   }
@@ -33,7 +35,7 @@ func main () {
   // expect src, destination , default to data/ scanDb.sqlite
 
   dataDir := "./data/"
-  outputName := "scanDb.sqlite"
+  outputName := "./scanDb.sqlite"
   switch len(args) {
     case 3:
       outputName = args[2]
@@ -42,42 +44,48 @@ func main () {
       dataDir = args[1]
   }
 
-  
-
-  //fix: warn or pause if already exists
+  // //fix: warn or pause if already exists
   os.Remove(outputName)
 
-  db, err = sql.Open("sqlite3", outputName)
+  db, err := sql.Open("sqlite3", outputName)
   check(err)
 
   defer db.Close()
 
   createHosts := `
-  create table hosts (id integer not null primary key
-                      name text
-                      errors int //bitmask
-                      keyExRSA bool
-                      keyExDHE bool
-                      keyExECDHE bool
-                      authRSA bool
-                      authAnon bool
-                      authDSA bool
-                      authEC bool
+  create table hosts (id integer not null primary key,
+                      name text,
+                      errors int,
+                      keyExRSA bool,
+                      keyExDHE bool,
+                      keyExECDHE bool,
+                      authRSA bool,
+                      authAnon bool,
+                      authDSA bool,
+                      authEC bool,
                       comments text 
                       )
   `
-  createHosts := `
-  create table handshakes (id integer not null primary key
-                           FOREIGN KEY (host) REFERENCES hosts
-                           cipher text
-                           keyexid int
-                           keyexbits int 
-                           keyexcurve string
-                           authid int
-                           uathbits int
-                           authcurve string
+  createHandshakes := `
+  create table handshakes (id integer not null primary key,
+                           host integer,
+                           cipher text,
+                           keyexid int,
+                           keyexbits int ,
+                           keyexcurve string,
+                           authid int,
+                           uathbits int,
+                           authcurve string,
+                           FOREIGN KEY(host) REFERENCES hosts(id)
                       )
   `
+  _, err = db.Exec(createHosts)
+  check(err)
+
+  _, err = db.Exec(createHandshakes)
+  check(err)
+
+  log.Println(dataDir, outputName)
 
   //todo, read 1m file and fill in name
 
