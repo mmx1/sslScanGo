f793e3e2a441034c4b42958d7fb9243ee4cdd1b2
Mark Xue <mark@mxue.org>
2017-05-26 13:22:11 -0700

    added table insert

--- sslScanAnalyzer.go
+++ sslScanAnalyzer.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-  // "encoding/json"
+  "encoding/json"
   "database/sql"
   // "fmt"
   _ "github.com/mattn/go-sqlite3"
-  // "io/ioutil"
+  "io/ioutil"
   "log"
   "os"
+  "time"
 )
 
 type ConnectionError int
 const (
-  connectionRefused ConnectionError = 1 << iota // == 0
-  sslError // == 2
+  connectionRefused ConnectionError = iota // == 0
+  sslError // == 1
   timeout
   invalidHostname
   connectionReset
@@ -23,6 +24,44 @@ const (
   dhRSAmismatch
   other
 )
+
+type KeyExchangeMethod int8
+const (
+  rsaKeyExch KeyExchangeMethod = 1 << iota
+  dhe //2
+  ecdhe //4
+  fixedECDH //8
+)
+
+type AuthMethod int8
+const (
+  rsaAuth AuthMethod = 1 << iota
+  anonymous
+  dsa
+  ec
+)
+
+type HandShakeResult struct {
+  Cipher string
+  KeyExchangeID int
+  KeyExchangeBits int
+  KeyExchangeCurve string //don't hard-code curve names and ask directly from OpenSSL
+  AuthKeyId int
+  AuthKeyBits int
+  AuthKeyCurve string
+}
+
+type ScanResult struct{
+  Id int
+  Error []ConnectionError
+  //golang doesn't have option sets (bitmasks). So....array of ints
+  KeyExchangeMethods KeyExchangeMethod
+  AuthMethods AuthMethod
+
+  Handshakes []HandShakeResult
+  Timestamp time.Time
+  Comments string //drop exceptions in here to filter later
+}
 
 func check(e error) {
   if e != nil {
@@ -74,7 +113,7 @@ func main () {
                            keyexbits int ,
                            keyexcurve string,
                            authid int,
-                           uathbits int,
+                           authbits int,
                            authcurve string,
                            FOREIGN KEY(host) REFERENCES hosts(id)
                       )
@@ -89,10 +128,54 @@ func main () {
 
   //todo, read 1m file and fill in name
 
-  // files, err = ioutil.ReadDir(dataDir)
-  // check(err)
-  // for _, f := range files {
-  //   f, err := io.
+  insertHostStmt, err := db.Prepare("insert into hosts (id, errors, keyExRSA, keyExDHE, keyExECDHE, authRSA, authAnon, authDSA, authEC, comments) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )")
+  check(err)
+  defer insertHostStmt.Close()
+  insertHandshakeStmt, err := db.Prepare("insert into handshakes (host, cipher, keyexid, keyexbits, keyexcurve, authid, authbits, authcurve ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?) ")
+  check(err) 
+  defer insertHandshakeStmt.Close()
 
-  // }
+  files, err := ioutil.ReadDir(dataDir)
+  check(err)
+  for _, f := range files {
+    log.Println(dataDir + f.Name())
+    file, err := os.Open(dataDir +  f.Name())
+    if err != nil {
+      log.Printf("Error reading file %s: %s \n", f, err)
+      continue
+    }
+    defer file.Close()
+
+    decoder := json.NewDecoder(file)
+    var result ScanResult
+    err = decoder.Decode(&result)
+    if err != nil {
+      log.Printf("Error reading json in file %s: %s \n", f, err)
+      continue
+    }
+
+    // log.Printf(result)
+
+    errorMask := 0
+    //make into a bitmask
+    for _ , element := range result.Error {
+      errorMask += 1 << uint(element)
+    }
+
+    //todo, export constants from serialze.go?
+    _, err = insertHostStmt.Exec( result.Id, 
+                        errorMask, 
+                        (result.KeyExchangeMethods & 1) != 0,  
+                        (result.KeyExchangeMethods & 2) != 0,
+                        (result.KeyExchangeMethods & 4) != 0,
+                        (result.AuthMethods & 1) != 0,
+                        (result.AuthMethods & 2) != 0,
+                        (result.AuthMethods & 4) != 0,
+                        (result.AuthMethods & 8) != 0,
+                        result.Comments )
+
+
+    //
+
+  }
 }
