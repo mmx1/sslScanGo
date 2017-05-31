fccb4d3df50c592900e9206a9859df3109530fa2
Mark Xue <mark@mxue.org>
2017-05-30 21:09:00 -0700

    batch insertions for increased speed

--- sslScanAnalyzer.go
+++ sslScanAnalyzer.go
@@ -86,61 +86,50 @@ func main () {
       dataDir = args[1]
   }
 
-  // //fix: warn or pause if already exists
-  os.Remove(outputName)
+  _, err := os.Stat(outputName)
+  dbExists := err == nil
 
   db, err := sql.Open("sqlite3", outputName)
   check(err)
-
   defer db.Close()
 
-  createHosts := `
-  create table hosts (id integer not null primary key,
-                      name text,
-                      errors int,
-                      keyExRSA bool,
-                      keyExDHE bool,
-                      keyExECDHE bool,
-                      authRSA bool,
-                      authAnon bool,
-                      authDSA bool,
-                      authEC bool,
-                      comments text 
-                      )
-  `
-  createHandshakes := `
-  create table handshakes (id integer not null primary key,
-                           host integer,
-                           cipher text,
-                           keyexid int,
-                           keyexbits int ,
-                           keyexcurve string,
-                           authid int,
-                           authbits int,
-                           authcurve string,
-                           FOREIGN KEY(host) REFERENCES hosts(id)
-                      )
-  `
-  createStatistics := `
-  create table stats (id integer not null primary key,
-                           totRowCount int,
-                           totKeyExRSA int,
-                           totKeyExDHE int,
-                           totKeyExECDHE int,
-                           totAuthRSA int,
-                           totAuthAnon int,
-                           totAuthDSA int,
-                           totAuthEC int
-                      )
-  `
-  _, err = db.Exec(createHosts)
-  check(err)
+  if !dbExists {
 
-  _, err = db.Exec(createHandshakes)
-  check(err)
-  _, err = db.Exec(createStatistics)
-  check(err)
+    createHosts := `
+    create table hosts (id integer not null primary key,
+                        name text,
+                        errors int,
+                        keyExRSA bool,
+                        keyExDHE bool,
+                        keyExECDHE bool,
+                        authRSA bool,
+                        authAnon bool,
+                        authDSA bool,
+                        authEC bool,
+                        comments text 
+                        )
+    `
+    createHandshakes := `
+    create table handshakes (id integer not null primary key,
+                             host integer,
+                             cipher text,
+                             keyexid int,
+                             keyexbits int ,
+                             keyexcurve string,
+                             authid int,
+                             authbits int,
+                             authcurve string,
+                             FOREIGN KEY(host) REFERENCES hosts(id)
+                        )
+    `
+    _, err = db.Exec(createHosts)
+    check(err)
 
+    _, err = db.Exec(createHandshakes)
+    check(err)
+
+  }
+  
   log.Println(dataDir, outputName)
 
   //todo, read 1m file and fill in name
@@ -149,71 +138,132 @@ func main () {
 
   files, err := ioutil.ReadDir(dataDir)
   check(err)
-  done := make(chan int, len(files))
-  var nW int
-  if len(files) < 20 {
-    nW = len(files)
-  } else {
-    nW = 20
-  }
-  worker := make (chan int, nW)
-  dbLock := make(chan int, 1)
-  dbLock <- 1
-  for i := 0; i < nW; i ++ {
-    worker <- 1
-  } 
-  for _, f := range files {
-    if f.Name() == "." {
-      log.Println("**** FOUND CURRENT DIR *****")
-    }
-    
-    //create function when worker is present
-    <-worker
-    go func(f os.FileInfo){
-      
-      defer func(){
-        worker <- 1
-        done <- 1
-        dbLock <- 1
-      }()
+  // done := make(chan int, len(files))
+  // var nW int
+  // if len(files) < 20 {
+  //   nW = len(files)
+  // } else {
+  //   nW = 20
+  // }
+  // worker := make (chan int, nW)
+  // dbLock := make(chan int, 1)
+  // dbLock <- 1
+  // for i := 0; i < nW; i ++ {
+  //   worker <- 1
+  // } 
 
-      log.Println(dataDir + f.Name())
-            
+  resultChan := make(chan ScanResult, 100)
+
+  //kick off thread to fill resultChan and close when done
+  go func () {
+    for i , f := range files {
+      if f.Name() == "." {
+        log.Println("**** FOUND CURRENT DIR *****")
+      }
+
+      if i % 10000 == 0 {
+        log.Println("Read ", i , dataDir + f.Name())
+      }
+
       file, err := os.Open(dataDir +  f.Name())
       if err != nil {
         log.Printf("Error reading file %s: %s \n", f, err)
-        return
+        continue
       }
-      defer file.Close()
+      //defer file.Close()
 
       decoder := json.NewDecoder(file)
       var result ScanResult
       err = decoder.Decode(&result)
+      file.Close()
       if err != nil {
         log.Printf("Error reading json in file %s: %s \n", f, err)
-        return
+        continue
       }
+
+      resultChan <- result
+    }
+    close(resultChan)
+  }()
+    
+  done := false
+  for !done {
+
+    tx, err := db.Begin()
+    check(err) 
+    insertHostStmt, err := tx.Prepare("insert into hosts (id, errors, keyExRSA, keyExDHE, keyExECDHE, authRSA, authAnon, authDSA, authEC, comments) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )")
+    check(err)
+    insertHandshakeStmt, err := tx.Prepare("insert into handshakes (host, cipher, keyexid, keyexbits, keyexcurve, authid, authbits, authcurve ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?) ")
+    check(err)
+
+    //batch up to 10 results
+    for i := 0 ; i < 10 ; i++ {
+      select {
+      case result, more := <- resultChan:
+        done = !more
+
+        errorMask := 0
+        //make into a bitmask
+        foundM := make (map[uint]bool)
+        for _ , element := range result.Error {
+          if !foundM[uint(element)]{
+            errorMask += 1 << uint(element)
+          }
+          foundM[uint(element)]=true
+        }
+        _, err = insertHostStmt.Exec( result.Id, 
+                          errorMask, 
+                          (result.KeyExchangeMethods & 1) != 0,  
+                          (result.KeyExchangeMethods & 2) != 0,
+                          (result.KeyExchangeMethods & 4) != 0,
+                          (result.AuthMethods & 1) != 0,
+                          (result.AuthMethods & 2) != 0,
+                          (result.AuthMethods & 4) != 0,
+                          (result.AuthMethods & 8) != 0,
+                          result.Comments )
+
+        for _, handshake := range result.Handshakes {
+          _, err = insertHandshakeStmt.Exec ( result.Id,
+                                              handshake.Cipher,
+                                              handshake.KeyExchangeID,
+                                              handshake.KeyExchangeBits,
+                                              handshake.KeyExchangeCurve,
+                                              handshake.AuthKeyId,
+                                              handshake.AuthKeyBits,
+                                              handshake.AuthKeyCurve )
+          check(err)
+        }
+      default: //if none ready
+        if i == 0 { //keep waiting if no insert yet
+          i--
+        }else{   //break and commit
+          continue
+        }
+      }
+    }
+      
+    err = tx.Commit()
+    check(err)
+    insertHandshakeStmt.Close()
+
+  }
+
+      
+      
+            
+      
+
+      
+
+
 
       // log.Printf(result)
 
-      errorMask := 0
-      //make into a bitmask
-      foundM := make (map[uint]bool)
-      for _ , element := range result.Error {
-        if !foundM[uint(element)]{
-          errorMask += 1 << uint(element)
-        }
-        foundM[uint(element)]=true
-      }
+      
 
       // For Concurrency Need A transaction for adding to the database
-      <-dbLock
-      tx, err := db.Begin()
-      if err != nil {
-        //log.Println("here begin1")
-      }
-      check(err) 
-      insertHostStmt, err := tx.Prepare("insert into hosts (id, errors, keyExRSA, keyExDHE, keyExECDHE, authRSA, authAnon, authDSA, authEC, comments) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )")
+      //<-dbLock
+      
       //counter := 0
       // GETTING A 'DATABASE IS LOCKED' ERROR 
       // THIS MEANS THAT "LIKELY" the thread holding the lock on the database was removed by the 
@@ -227,23 +277,12 @@ func main () {
       //   insertHostStmt, err = tx.Prepare("insert into hosts (id, errors, keyExRSA, keyExDHE, keyExECDHE, authRSA, authAnon, authDSA, authEC, comments) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )")
       //   counter++
       // }
-      if err != nil {
-        log.Println("here prep1")
-      }
-      check(err)
+
+      //check(err)
 
       
       //todo, export constants from serialze.go?
-      _, err = insertHostStmt.Exec( result.Id, 
-                          errorMask, 
-                          (result.KeyExchangeMethods & 1) != 0,  
-                          (result.KeyExchangeMethods & 2) != 0,
-                          (result.KeyExchangeMethods & 4) != 0,
-                          (result.AuthMethods & 1) != 0,
-                          (result.AuthMethods & 2) != 0,
-                          (result.AuthMethods & 4) != 0,
-                          (result.AuthMethods & 8) != 0,
-                          result.Comments )
+      
       // COMMITS THE TRANSACTION TO THE DATABASE (RELEASES THE DATABASE LOCK)
       // ALSO anything tied (state: i.e. insertHostStmt) to this transaction is deleted
       // err = tx.Commit()
@@ -263,33 +302,10 @@ func main () {
       //   //log.Println("here begin2")
       // }
       // check(err)
-      insertHandshakeStmt, err := tx.Prepare("insert into handshakes (host, cipher, keyexid, keyexbits, keyexcurve, authid, authbits, authcurve ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?) ")
-      if err != nil {
-        //log.Println("here prep2")
-      }
-      check(err) 
-      
-      for _, handshake := range result.Handshakes {
-        _, err = insertHandshakeStmt.Exec ( result.Id,
-                                            handshake.Cipher,
-                                            handshake.KeyExchangeID,
-                                            handshake.KeyExchangeBits,
-                                            handshake.KeyExchangeCurve,
-                                            handshake.AuthKeyId,
-                                            handshake.AuthKeyBits,
-                                            handshake.AuthKeyCurve )
-        check(err)
 
-      }
-      err = tx.Commit()
-      check(err)
-      insertHandshakeStmt.Close()
-
-    }(f)
-
-  }
-  for i := 0; i < len(files); i++ {
-    <-done
-  }
+  //}
+  // for i := 0; i < len(files); i++ {
+  //   <-done
+  // }
   log.Println("Finished the insert of all files: ", len(files))
 }
